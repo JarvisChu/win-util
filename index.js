ff1bd955ef104886da317127958f283517fab9a1
zhujiangtao <jarvischu@126.com>
2021-08-03 10:10:24 +0800

    v0.0.4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var addon = require('bindings')('win-util-addon.node')
 
 module.exports = function(){
-  this.RegisterWindowMessage = function(...props){
+  let RegisterWindowMessage = function(...props){
     return addon.RegisterWindowMessageFunc(...props)
   }
-  return this
+  return {RegisterWindowMessage}
 }
